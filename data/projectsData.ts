8ef6490606d79cfceca7e89841e1c86dc6a6b0b5
Bill Tsui <b10902118@ntu.edu.tw>
2025-12-11 15:50:28 +0800

    feat: update content

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Chrome Inspector JS',
+    description: `An intuitive programming interface for Chrome Inspector — bringing Elements and Styles data out of DevTools!`,
+    imgSrc: '',
+    href: 'https://github.com/devtoolcss/chrome-inspector',
   },
 ]
 
